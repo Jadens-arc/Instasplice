75446c6d3bf3386426cb2505cfe592121ccbfb1b
Jaden Arceneaux <mining.mojo@icloud.com>
2021-04-13 17:48:50 -0700

    Init

---
+++ SplitImage.py
@@ -0,0 +1,47 @@
+from PIL import Image
+
+
+def splitImage(n, path=None, image=None):
+    if image == None:
+        image = Image.open(path)
+    width, height = image.size
+    images = []
+    left = 0
+    for i in range(n):
+        images.append(image.crop((left, 0, left + (width / n), height)))
+        left += width / n
+
+    return images
+
+
+def saveImages(images, name, filetype):
+    for index, image in enumerate(images):
+        image.save(f'output/{name}{index}.{filetype}')
+
+
+def makeImageDivisible(path):
+    image = Image.open(path)
+    width, height = image.size
+    leftOver = width % height
+    print(leftOver)
+    return image.crop((leftOver / 2, 0, width - (leftOver / 2), height))
+
+
+def autosplit(path, save=True):
+    croppedImage = makeImageDivisible(path)
+    width, height = croppedImage.size
+    ratio = width / height
+    images = splitImage(int(ratio), image=croppedImage)
+
+    if save:
+        pathInfo = path.split('.')
+        pathInfo[0] = pathInfo[0].split('/')[-1]
+        saveImages(images, pathInfo[0], pathInfo[1])
+
+    return images
+
+
+if __name__ == '__main__':
+    # images = splitImage('assets/testimage1.png', 3)
+    # saveImages(images, 'lol', 'png')
+    autosplit('assets/lake.jpg')
