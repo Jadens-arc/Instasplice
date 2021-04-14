3f05f6c54c4aa957773b2cb0dc410a6018a7a006
Jaden Arceneaux <mining.mojo@icloud.com>
2021-04-14 12:44:43 -0700

    formatting and added button to view images

--- main.py
+++ main.py
@@ -6,27 +6,33 @@ root = tk.Tk()
 root.title('Instasplice')
 root.geometry('300x200')
 
+
 def browseFiles():
     filename = filedialog.askopenfilename(
-    	initialdir = "/",
-    	title = "Select a File",
-    	filetypes = (
-			("all files", "*.*"),
-    		("JPG files", "*.jpg*"),
-    		("PNG files", "*.png*"),
-    		("JPEG", "*.jpeg*"),
-		)
+        initialdir="/",
+        title="Select a File",
+        filetypes=(
+            ("all files", "*.*"),
+            ("JPG files", "*.jpg*"),
+            ("PNG files", "*.png*"),
+            ("JPEG", "*.jpeg*"),
+        )
     )
     return filename
 
-def cropImage():
-	path = browseFiles()
-	autosplit(path)
 
-      
+def cropImage():
+    path = browseFiles()
+    if path:
+        autosplit(path)
+        viewPhotosBtn = tk.Button(root, text="View Photos")
+        viewPhotosBtn.place(relx=0.1, rely=0.5, relwidth=0.8)
+
+
+title = tk.Label(root, text="Pick an image to crop")
+title.place(relx=0.1, rely=0.3, relwidth=0.8)
 
 selectButton = tk.Button(root, text="Select File", command=cropImage)
 selectButton.place(relx=0.1, rely=0.4, relwidth=0.8)
 
 root.mainloop()
-
